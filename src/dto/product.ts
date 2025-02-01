2a0186354f7d5b6815d52009b851501762bd0c19
umer-2k1 <umermemon4648@gmail.com>
2025-02-01 23:09:52 +0500

    chore: update docs url

--- src/dto/product.ts
+++ src/dto/product.ts
@@ -1,28 +1,36 @@
-import { IsString, IsNotEmpty, IsInt, Min, isNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, IsInt, Min } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   title: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   desc: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   category: string;
 
+  @ApiProperty()
   @IsInt()
   @Min(1, { message: 'Quantity must be at least 1' })
   qty: number;
 
+  @ApiProperty()
   @IsInt()
   price: number;
 
+  @ApiProperty()
   @IsString()
   images: string[];
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   brand: string;
--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .addTag('cats')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
   await app.listen(3000);
 }
